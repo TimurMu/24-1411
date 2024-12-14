ec49b6c37f0f2a58fa3bde1ed5bb248acc83d508
TimurMu <87207341+TimurMu@users.noreply.github.com>
2024-12-14 16:20:12 +0300

    Update Defence_1.jl

--- Defence_1.jl
+++ Defence_1.jl
@@ -4,88 +4,9 @@ using HorizonSideRobots
 r = Robot(animate=true)
 inverse(side::HorizonSide) = HorizonSide((Int(side)+2)%4)
 
-
-#задача 4
-#робот делает косой крест
-
-function xcross!(robot)
-    for s = ((Nord, Ost), (Sud, Ost), (Sud, West), (Nord, West))
-        n = xmark_direct!(robot, s)
-        xmove!(robot, inverse.(s), n)
-    end
-end
-
-function xmove!(robot, side, num_steps::Integer)
-    for _ in 1:num_steps
-        move!(robot, side[1])
-        move!(robot, side[2])
-    end
-end
-
-function xmark_direct!(robot, side::NTuple{2, HorizonSide})
-    n = 0
-    while !(isborder(robot, side[1]) || isborder(robot, side[2]))
-        #перегородки нет с обоих направлений
-        move!(robot, side[1])
-        move!(robot, side[2])
-        n = n + 1
-        putmarker!(robot)
-    end
-    return n
-end
-
-
-#задача 5
-#робот обходит центральный прямоугольник
-
-
-function opheilalalei!(robot) #финальная функция 
-    while !isborder(robot, Sud) && !isborder(robot, West)
-        do_upora_shagay_bistro!(robot, Sud)
-        do_upora_shagay_bistro!(robot, West)
-    end
-
-    for side in [(Ost, Nord), (Sud, Ost)]
-        slim_shady_gooo!(robot, side)
-    end
-
-end
-
-function do_upora_shagay_bistro!(robot, side) #доход до стенки без отметок
-    while !isborder(robot, side)
-        move!(robot, side)
-    end
-end
-
-
-function do_upora_emaie!(robot, side) #доход до стенки с отметками
-    while !isborder(robot, side)
-        for n = (Nord, Ost, West, Sud)
-            if isborder(robot, n)
-                putmarker!(robot)
-            end
-        end
-        move!(robot, side)
-    end
-end
-
-
-function slim_shady_gooo!(robot, side::NTuple{2, HorizonSide})
-    while !isborder(robot, side[2])
-        slice = side[1]
-        do_upora_emaie!(robot, slice)
-        putmarker!(robot)
-        move!(robot, side[2])
-        slice = inverse(slice)
-        do_upora_emaie!(robot, slice)
-    end
-end
-
-
-
-
 #задача 6 a
 #робот обхрдит все препятствия и ставит маркеры у внешнего барьера 
+
 
 
 function square!(robot) #проход по квадрату
@@ -139,11 +60,8 @@ function HorizonSideRobots.move!(robot, side, num_steps::Integer)
 end
 
 
-
-
 #задача 6 b
 #робот обходит все препятствия и ставит маркеры у внешнего барьера 
-
 
 function square!(robot, x, y) #проход по квадрату
     for s in (Ost, Sud, West, Nord)
@@ -230,9 +148,180 @@ function marshrut_blin!(robot, side) #делает маршрут
     
 end
 
+function HorizonSideRobots.move!(robot, side, num_steps::Integer)
+    for _ in 1:num_steps
+        move!(robot, side)
+    end
+end
+
+
+#задача 8
+
+function chetaem_shagi!(robot, side, p)  #до стенки если p == 1 то будет маркировать и возвращает кол-во шагов
+    s = 0
+    while !isborder(robot, side) 
+        move!(robot, side)
+        if p == 1
+            putmarker!(robot)
+        end
+        s = s + 1
+    end
+    return s
+end
+
+
+function marshrut_blin!(robot, side) #делает маршрут
+    
+    i = 1.0
+    while !ismarker(robot)
+        move!(robot, side, Int(floor(i)))
+        i += 0.5
+        side = inverse(side)
+    end
+    
+end
+
 
 function HorizonSideRobots.move!(robot, side, num_steps::Integer)
     for _ in 1:num_steps
         move!(robot, side)
+    end
+end
+
+
+# Задание 9
+
+mutable struct R
+    robot::Robot
+    flag::Bool
+end
+
+RR = R(robot, true)
+
+function HorizonSideRobots.move!(robot::R, side)
+    
+    if robot.flag
+        putmarker!(robot.robot)
+    end
+
+    if !isborder(robot.robot, side)
+        move!(robot.robot, side)
+        robot.flag = !robot.flag
+    end
+
+end
+
+
+function do_upora_emai!(robot, side)
+    while !isborder(robot.robot, side)    
+        move!(robot, side)
+    end
+end
+
+function chetaem_shagi!(robot, side)
+    n = 0
+    while !isborder(robot.robot, side)
+        robot.flag = false    
+        move!(robot, side)
+        n += 1
+    end
+    return n
+end
+
+
+function snake_mini_kapetz!(robot)
+    y = chetaem_shagi!(robot, Sud)
+    x = chetaem_shagi!(robot, West)
+    side = Ost
+
+    if x % 2 == 0
+        robot.flag = !robot.flag
+    end
+
+    while !isborder(robot.robot, Nord)
+        do_upora_emai!(robot, side)
+        move!(robot, Nord)
+        side = inverse(side)
+    end
+
+    do_upora_emai!(robot, side)
+    do_upora_emai!(robot, Sud)
+    do_upora_emai!(robot, West)
+    
+    for i in 1:x
+        move!(robot, Ost) 
+    end
+
+    for j in 1:y
+        move!(robot, Nord) 
+    end
+end
+
+
+# Задание 10
+
+mutable struct R
+    robot::Robot
+    flag::Bool
+end
+
+RR = R(robot, true)
+
+function HorizonSideRobots.move!(robot::R, side)
+    
+    if robot.flag
+        putmarker!(robot.robot)
+    end
+
+    if !isborder(robot.robot, side)
+        move!(robot.robot, side)
+        robot.flag = !robot.flag
+    end
+
+end
+
+
+function do_upora_emai!(robot, side)
+    while !isborder(robot.robot, side)    
+        move!(robot, side)
+    end
+end
+
+function chetaem_shagi!(robot, side)
+    n = 0
+    while !isborder(robot.robot, side)
+        robot.flag = false    
+        move!(robot, side)
+        n += 1
+    end
+    return n
+end
+
+
+function snake_mini_kapetz!(robot)
+    y = chetaem_shagi!(robot, Sud)
+    x = chetaem_shagi!(robot, West)
+    side = Ost
+
+    if x % 2 == 0
+        robot.flag = !robot.flag
+    end
+
+    while !isborder(robot.robot, Nord)
+        do_upora_emai!(robot, side)
+        move!(robot, Nord)
+        side = inverse(side)
+    end
+
+    do_upora_emai!(robot, side)
+    do_upora_emai!(robot, Sud)
+    do_upora_emai!(robot, West)
+    
+    for i in 1:x
+        move!(robot, Ost) 
+    end
+
+    for j in 1:y
+        move!(robot, Nord) 
     end
 end
