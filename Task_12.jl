c9794fbd2ead5358e3911b2068104f9a623e9746
TimurMu <87207341+TimurMu@users.noreply.github.com>
2024-12-17 12:30:26 +0300

    Add files via upload

--- Task_12.jl
+++ Task_12.jl
@@ -1,74 +1,52 @@
-#12
-
-
 using HorizonSideRobots
-r = Robot(animate=true)
 
-mutable struct CheckRobot
+
+robot = Robot(animate = true)
+inverse(side::HorizonSide)=HorizonSide((Int(side)+2)%4)
+
+
+s = 0
+mutable struct R
     robot::Robot
-    f::Bool
-    f_ch::Bool
+    flag::Bool
 end
 
-cr=CheckRobot(r,false,false)
+RR = R(robot, false)
 
-function count_dck!(robot)
-    num_steps_West = do_predela!(robot.robot, West)
-    num_steps_Sud = do_predela!(robot.robot, Sud)
-    n = podschet!(robot)
-    do_predela!(robot.robot, West)
-    do_predela!(robot.robot, Sud)
-    move!(robot.robot, Nord, num_steps_Sud)
-    move!(robot.robot, Ost, num_steps_West)
-    return n
-end
 
-function HorizonSideRobots.move!(robot, side, num_steps)
-    for _ in 1:num_steps
-        move!(robot, side)
+function HorizonSideRobots.move!(robot::R, side)
+    
+    if robot.flag
+        putmarker!(robot.robot)
     end
+
+    move!(robot.robot, side)
+
 end
 
-function do_predela!(robot, side)
-    n = 0
-    while !isborder(robot, side)
-        move!(robot, side)
-        n += 1
-    end
-    return n
-end
 
-function podschet!(robot)
-    s = Ost
-    n = 0
-    while true
-        n += p_line(robot, s)
-        s = inverse!(s)
-        isborder(robot.robot, Nord) && return n-1
-        move!(robot.robot, Nord)
-    end
-end
-
-function inverse!(side::HorizonSide)
-    return HorizonSide((Int(side) + 2)%4)
-end
-
-function p_line(robot, side)
-    n = 0
-    robot.f_ch = false
-    while true
-        move!(robot.robot, side)
-        if isborder(robot.robot, Nord)
-            !robot.f && (n+=1)
-            robot.f = true
-            robot.f_ch = true
-        else
-            #!robot.f_ch && (robot.f=false)
-            if !robot.f_ch
-                robot.f = false
+function chetaem_ne_stenki_gadem!(robot, side)
+    countwall = 0 
+    current = 0
+    isWall = false
+    while !isborder(robot.robot, side)
+        if !isborder(robot.robot, Nord)
+            current += 1
+            if current > 1 && isWall
+                countwall += 1
+                isWall = false
             end
-            robot.f_ch = false
+        else
+            isWall = true
+            current = 0
         end
-        isborder(robot.robot, side) && return n
+        move!(robot.robot, side)
     end
+    return countwall
 end
+
+
+function snake_mini_kapetz!(robot)
+    total_count = 0
+end
+
