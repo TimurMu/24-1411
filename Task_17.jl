5a2acd97a25b8b766b6f8740ab187dfecb40f8f6
TimurMu <87207341+TimurMu@users.noreply.github.com>
2024-12-20 14:44:27 +0300

    Add files via upload

---
+++ Task_17.jl
@@ -0,0 +1,28 @@
+using HorizonSideRobots
+robot = Robot(animate = true)
+
+
+function find_direct!(stop_condition::Function, robot, side, nmax_steps)
+    n=0
+    while !(stop_condition() || (n == nmax_steps))
+        move!(robot, side)
+        n+=1
+    end
+    return stop_condition()
+end
+
+function find!(robot)
+    spiral!(()->ismarker(robot), robot)
+end
+
+function spiral!(stop_condition::Function, robot)
+    nmax_steps = 1
+    s = Nord
+    while !find_direct!(stop_condition, robot, s, nmax_steps)
+        (s in (Nord, Sud)) && (nmax_steps += 1)
+        s = left(s)
+    end
+end
+
+
+left(side) = HorizonSide((Int(side)+1)%4)
